9d97206e58e6bf9e4ba68f5bce7fe84ef86bea2d
Kadir İldeniz <kadirildeniz5@gmail.com>
2021-01-27 01:11:49 +0300

    Update basicPortscanner.py

--- basicPortscanner.py
+++ basicPortscanner.py
@@ -6,7 +6,7 @@ print("End Port Number :")
 startPort = input()
 for i in range(startPort):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(2)  # 2 Second Timeout
+    sock.settimeout(2)
     result = sock.connect_ex(('192.168.1.1', i))
     if result == 0:
         print("Port open , open port {0}".format(i))
