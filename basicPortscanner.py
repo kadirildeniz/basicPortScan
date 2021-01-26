982603086f86115b9c082d3b465dc3e75212ff4f
Kadir İldeniz <kadirildeniz5@gmail.com>
2021-01-27 02:58:39 +0300

    Update basicPortscanner.py

--- basicPortscanner.py
+++ basicPortscanner.py
@@ -1,16 +1,17 @@
-import os
-import socket
-import time
+def portScan():
+    import socket
+    print("enter ip address")
+    ipAddress = raw_input()
+    print("End Port Number :")
+    startPort = input()
+    for i in range(startPort):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(2)
+        result = sock.connect_ex(('{0}'.format(ipAddress), i))
+        if result == 0:
+            print("Port open , open port {0}".format(i))
+        else:
+            print("Port close , close port {0}".format(i))
 
-print("End Port Number :")
-startPort = input()
-for i in range(startPort):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(2)
-    result = sock.connect_ex(('192.168.1.1', i))
-    if result == 0:
-        print("Port open , open port {0}".format(i))
-    else:
-        print("Port close , close port {0}".format(i))
 
-
+portScan()
