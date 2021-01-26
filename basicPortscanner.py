a6cd7edd2c50e2d987bd9d74c4b0541e22a130ea
Kadir İldeniz <kadirildeniz5@gmail.com>
2021-01-27 00:59:21 +0300

    Update basicPortscanner.py

--- basicPortscanner.py
+++ basicPortscanner.py
@@ -1,16 +1,22 @@
 import os
+import socket
 import time
 
-print("Port Number :")
+print("End Port Number :")
 startPort = input()
 operatingSystem = os.name
 for i in range(startPort):
     if operatingSystem == "posix":
         outputPortScan = os.system("nc -z -v 192.168.1.1 {0}".format(i))
     elif operatingSystem == "nt":
-        print("!!")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(2)  # 2 Second Timeout
+        result = sock.connect_ex(('192.168.1.1', i))
+        if result == 0:
+            print 'port OPEN'
+        else:
+            print 'port CLOSED, connect_ex returned: ' + str(result)
     else:
         print "not support OS "
-
 
 
